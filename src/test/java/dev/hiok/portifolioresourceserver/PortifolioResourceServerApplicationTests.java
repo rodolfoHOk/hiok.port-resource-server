6a323e33468f94a1d11de5b7cd28cd9528d02478
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-05-31 10:37:33 -0300

    create with spring boot initalizr

---
+++ src/test/java/dev/hiok/portifolioresourceserver/PortifolioResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hiok.portifolioresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
